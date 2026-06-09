bd502740a2b0eda3a7236f846ac52b3706658032
vishnu1406vimala-afk <vishnu1406vimala@gmail.com>
2026-06-09 14:14:27 +0530

    Create portfolio.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/portfolio.java
@@ -0,0 +1,33 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String portfolioName;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+}
